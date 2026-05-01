00d5688aa2522e17199c74d3683d83b7c2dc6873
Raunak Kumar Tiwari <raunaktiwari1729@gmail.com>
2026-05-01 15:58:46 +0000

    dates added

---
+++ file1/dates.js
@@ -0,0 +1,7 @@
+let myDate=new Date()
+
+console.log(myDate.toLocaleString()) 
+
+let createdDate=new Date(2025,9,2)
+
+console.log(createdDate.toDateString())
--- file1/num_maths.js
+++ file1/num_maths.js
@@ -1,9 +1,19 @@
-const score=100
+// const score=100
 
-console.log(score)
+// console.log(score)
 
-const balance=new Number(100)
+// const balance=new Number(100)
 
-console.log(balance)
+// console.log(balance)
 
-console.log(typeof balance.toString())
+// console.log(balance.toString().length)
+
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+console.log((Math.random()*100)+6)
+max=100
+min=50
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
