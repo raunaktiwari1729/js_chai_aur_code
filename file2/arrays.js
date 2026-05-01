c9f33f9171ac263ba28db5b072697553f9d9dd1d
Raunak Kumar Tiwari <raunaktiwari1729@gmail.com>
2026-05-01 16:11:43 +0000

    arrays

---
+++ file2/arrays.js
@@ -0,0 +1,19 @@
+const arr=[45,98,"Raunak",true]
+
+console.log(arr)
+
+let myArr=new Array(6,7)
+
+console.log(myArr)
+
+arr.push(8)
+myArr.push(8)
+
+console.log(myArr)
+console.log(arr)
+
+console.log(arr.unshift(67))//not optimised
+
+console.log(arr)
+
+//slice and splice
